6d0a33a28e8b8fa86e774fe3554dfb6044ccb61c
kel097-sketch <kel097@ucsd.edu>
2025-11-29 22:56:23 -0800

    Update main.py

--- data_cleaning/main.py
+++ data_cleaning/main.py
@@ -1,5 +1,4 @@
 from apps_cleaning import clean_googleplay_apps, apps_basic_stats
-from data_visualisation import create_analysis_dashboard
 
 if __name__ == "__main__":
     FIG_REV = "figs/reviews"
@@ -8,7 +7,5 @@ if __name__ == "__main__":
     df_apps, rep_apps = clean_googleplay_apps(
         IN_APPS, out_csv=OUT_APPS, keep_latest_per_app=True
     )
-
-    create_analysis_dashboard(df_apps,FIG_REV)
     stats_apps = apps_basic_stats(df_apps)
     print(stats_apps.to_string(index=False))
